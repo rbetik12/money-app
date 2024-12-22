a33a0bd98e1f32ce8170bf6a96a5e026a5c673c5
Vitaliy Prikota <vitaliy.prikota@playrix.com>
2024-12-22 22:13:25 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Vitaliy Prikota on 22.12.24..
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
